98b53fec39cb023a260db33bb7fd7a140383a59f
SamrudhR06 <samrudhrajeev2006@gmail.com>
2026-08-04 20:31:35 +0530

    Initial commit: setting up attendance calculator project

---
+++ style.css
@@ -0,0 +1,16 @@
+body {
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  max-width: 400px;
+  margin: 40px auto;
+}
+
+.input-group {
+  margin-bottom: 12px;
+}
+
+.result {
+  margin-top: 15px;
+}
